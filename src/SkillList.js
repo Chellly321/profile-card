65804fd3a067c3e5635ad897222d708717e2027d
Chelly321 <michelle.royer@icloud.com>
2023-10-20 15:47:30 +0200

    finished-demo

--- src/SkillList.js
+++ src/SkillList.js
@@ -1,14 +1,15 @@
 import React from "react";
+import data from "./data";
 import Skill from "./Skill";
 
 function SkillList() {
+  const skill = data;
+
   return (
     <div className="skill-list">
-      <Skill skill="React" emoji="✓" color="red" />
-      <Skill skill="HTML&CSS" emoji="✓" color="orange" />
-      <Skill skill="Javascript" emoji="✓" color="pink" />
-      <Skill skill="Firebase" emoji="✓" color="yellow" />
-      <Skill skill="SCRUM" emoji="✓" color="green" />
+      {skill.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    skill: "HTML&CSS",
+    level: "advanced",
+    color: "#92D6D5",
+  },
+  {
+    skill: "React",
+    level: "intermediate",
+    color: "#766ECC",
+  },
+  {
+    skill: "Scrum",
+    level: "intermediate",
+    color: "#C6C3E6",
+  },
+  {
+    skill: "Firebase",
+    level: "beginner",
+    color: "#C3E6D8",
+  },
+  {
+    skill: "Figma",
+    level: "advanced",
+    color: "#FBA844",
+  },
+];
+export default data;
